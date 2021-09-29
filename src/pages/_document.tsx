23968f5c604036f2ff08c1bb286a460d16a73117
Gabriel Borges <gbdesigns13@gmail.com>
2021-09-29 16:15:50 -0300

    Meta remove

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,6 +48,19 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
+          <meta
+            name="description"
+            content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+          />
+          <meta property="og:image" content="ogimage.png" />
+          <meta property="og:image:secure_url" content="ogimage.png" />
+          <meta name="twitter:image" content="ogimage.png" />
+          <meta name="twitter:image:src" content="ogimage.png" />
+          <meta
+            property="og:description"
+            content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+          />
+
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
